e835acf4d4022d18757bd2525dcd9307858d1fa0
David Fried <david.d.fried@gmail.com>
2020-11-12 22:41:24 -0600

    upload file and model predictions are working; need to format

--- website/app.py
+++ website/app.py
@@ -4,31 +4,36 @@ import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
 import numpy as np
+import matplotlib.pyplot as plt
+from skimage.transform import resize
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128,128,3)))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Conv2D(filters=4, kernel_size=2, padding='same',
+                 activation='relu', input_shape=(400, 400, 3)))
+model.add(MaxPooling2D(pool_size=2))
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Conv2D(filters=8, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.1))
 
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Conv2D(filters=12, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.3))
 
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(2, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-model.load_weights('static/model.h5')
+
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.4))
+
+model.add(Dense(3, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.load_weights('static/model/final_model.hdf5')
 
 
 COUNT = 0
@@ -39,37 +44,35 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
 def man():
     return render_template('index.html')
 
-
+###########upload images page###########
 @app.route('/uploadImage')
 def uploadImage():
     return render_template('uploadImage.html')
 
 
+###########address form###########
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        address = request.form["address"]
+        return redirect("/", code=302)
+    return render_template("form.html")
+
 @app.route('/prediction', methods=['POST'])
 def prediction():
     global COUNT
     img = request.files['image']
-
-    img.save('static/{}.jpg'.format(COUNT))    
-    img_arr = cv2.imread('static/{}.jpg'.format(COUNT))
-
-    img_arr = cv2.resize(img_arr, (128,128))
-    img_arr = img_arr / 255.0
-    img_arr = img_arr.reshape(1, 128,128,3)
-    prediction = model.predict(img_arr)
-
-    x = round(prediction[0,0], 2)
-    y = round(prediction[0,1], 2)
-    preds = np.array([x,y])
+    img.save(f'static/{COUNT}.jpg')    
+    image = plt.imread(f'static/{COUNT}.jpg')
+    resized_image = resize(image, (400,400,3))
+    preds = model.predict(np.array([resized_image]))
     COUNT += 1
     return render_template('prediction.html', data=preds)
-
 
 @app.route('/load_img')
 def load_img():
     global COUNT
-    return send_from_directory('static', "{}.jpg".format(COUNT-1))
-
+    return send_from_directory('static', f"{COUNT-1}.jpg")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/archive/app.py
+++ website/archive/app.py
@@ -1,40 +1,74 @@
-from flask import Flask, render_template, redirect, jsonify, request
-# import datetime as dt
-# import sqlalchemy
-# import pandas as pd
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine
+from flask import Flask, render_template, request, send_from_directory
+import cv2
+import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
+import numpy as np
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128,128,3)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.load_weights('static/model.h5')
 
 
-# # creating a SQL Alchemy ORM
-# engine = create_engine("sqlite:///database/database.sqlite")
-# Base = automap_base()
-# Base.prepare(engine, reflect=True)
-
-
-# # table names
-# ufo_data = Base.classes.ufo_data
-# state_stats = Base.classes.state_stats
-# merge_again = Base.classes.merge_again
-# shape_counts = Base.classes.shape_counts
-
+COUNT = 0
 app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
 
-
-########Home Page###########
-
-@app.route("/")
-def welcome():
+@app.route('/')
+def man():
     return render_template('index.html')
-    #return render_template('index.html', data=big_list)
 
 
-########Improve the Model###########
+@app.route('/uploadImage')
+def uploadImage():
+    return render_template('uploadImage.html')
 
-@app.route("/improveModel")
-def circlepack():
-    return render_template('/improveModel.html')
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    global COUNT
+    img = request.files['image']
+
+    img.save('static/{}.jpg'.format(COUNT))    
+    img_arr = cv2.imread('static/{}.jpg'.format(COUNT))
+
+    img_arr = cv2.resize(img_arr, (128,128))
+    img_arr = img_arr / 255.0
+    img_arr = img_arr.reshape(1, 128,128,3)
+    prediction = model.predict(img_arr)
+
+    x = round(prediction[0,0], 2)
+    y = round(prediction[0,1], 2)
+    preds = np.array([x,y])
+    COUNT += 1
+    return render_template('prediction.html', data=preds)
+
+
+@app.route('/load_img')
+def load_img():
+    global COUNT
+    return send_from_directory('static', "{}.jpg".format(COUNT-1))
 
 
 if __name__ == '__main__':
@@ -42,119 +76,3 @@ if __name__ == '__main__':
 
 
 
-
-
-
-# Example Code for SQL Alchemy
-
-# ########Bar Graph###########
-
-# @app.route("/bar")
-# def bar():
-#     return render_template('/bar.html')
-
-
-# @app.route("/bardata")
-# def bardata():
-    
-#     session = Session(engine)
-
-#     from flask import jsonify
-
-#     results = session.query(
-#     shape_counts.State,
-#     shape_counts.Changing,
-#     shape_counts.Chevron,
-#     shape_counts.Cigar,
-#     shape_counts.Circle,
-#     shape_counts.Cone,
-#     shape_counts.Cross,
-#     shape_counts.Cylinder,
-#     shape_counts.Diamond,
-#     shape_counts.Disk,
-#     shape_counts.Egg,
-#     shape_counts.Fireball,
-#     shape_counts.Flash,
-#     shape_counts.Formation,
-#     shape_counts.Light,
-#     shape_counts.Other,
-#     shape_counts.Oval,
-#     shape_counts.Rectangle,
-#     shape_counts.Sphere,
-#     shape_counts.Teardrop,
-#     shape_counts.Triangle,
-#     shape_counts.Unknown).all()
-
-#     session.close()
-
-#     big_list = []
-
-#     for result in results:
-
-#         obj = {
-#         'State': result[0],
-#         'Changing': result[1],
-#         'Chevron': result[2],
-#         'Cigar': result[3],
-#         'Circle': result[4],
-#         'Cone': result[5],
-#         'Cross': result[6],
-#         'Cylinder': result[7], 
-#         'Diamond': result[8],
-#         'Disk': result[9],
-#         'Egg': result[10],
-#         'Fireball': result[11],
-#         'Flash': result[12],
-#         'Formation': result[13],
-#         'Light': result[14],
-#         'Other': result[15],
-#         'Oval': result[16],
-#         'Rectangle': result[17],
-#         'Sphere': result[18],
-#         'Teardrop': result[19],
-#         'Triangle': result[20],
-#         'Unknown': result[21]
-#         }
-        
-#         big_list.append(obj)
-
-#     data=jsonify(big_list)
-    
-#     return data
-
-
-# ########Explore Data###########
-
-# @app.route("/explore")
-# def explore():
-#     return render_template('explore.html')
-
-
-# @app.route("/exploredata")
-# def exploredata():
-  
-#     session = Session(engine)
-
-#     from flask import jsonify
-
-#     results = session.query(
-#         ufo_data.datetime,
-#         ufo_data.city,
-#         ufo_data.state,
-#         ufo_data.country,
-#         ufo_data.shape,
-#         ufo_data.duration,
-#         ufo_data.duration_hours,
-#         ufo_data.comments,
-#         ufo_data.date_posted,
-#         ufo_data.latitude,
-#         ufo_data.longitude,
-#         ufo_data.year,
-#         ufo_data.month)\
-#         .filter((ufo_data.year == 2014) & (ufo_data.month < 3)).all()
-
-#     session.close()
-
-#     data=jsonify(results)
-
-#     return data
